d6a7d96428c7aa48af872b079398896fad36adf0
Nicolas Rodriguez <nico.fr98@gmail.com>
2022-01-08 14:07:33 -0300

    desafio 1: Armar navbar

---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function NavBar(){
+
+    return(
+        <>
+        <div>
+            <a href="https://">Ecommerce Nicolas</a>
+        </div>
+        <ul>
+            <li> <a href="https://">Home</a></li>
+            <li><a href="https://">Categories</a></li>
+            <li><a href="https://">On Sale</a></li>
+            <li><a href="https://">Shopping Cart</a></li>
+        </ul>
+    
+    
+        </>
+    )
+
+};
+
